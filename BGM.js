167ac8e45ae0ae17dc7dd41f4799f3eb03130af3
jeje <idith80@gmail.com>
2023-06-20 18:10:35 +0200

    Create BGM.js

---
+++ BGM.js
@@ -0,0 +1,5 @@
+{
+"begins" : "https://github.com/asrher/BGM/blob/main/m1.mp3?raw=true",
+ "sparrow": "https://github.com/asrher/BGM/blob/main/m2.mp3?raw=true",
+ "sorry": "https://github.com/asrher/BGM/blob/main/m3.mp3?raw=true"
+}
